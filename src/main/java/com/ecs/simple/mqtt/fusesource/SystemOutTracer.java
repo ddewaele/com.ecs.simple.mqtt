c631fae7856a335423c6172bd089106d1f3d9bc3
Davy De Waele <ddewaele@gmail.com>
2014-05-09 10:25:18 +0200

    updated tracer / logging

--- src/main/java/com/ecs/simple/mqtt/fusesource/SystemOutTracer.java
+++ src/main/java/com/ecs/simple/mqtt/fusesource/SystemOutTracer.java
@@ -1,22 +1,26 @@
 package com.ecs.simple.mqtt.fusesource;
 import org.fusesource.mqtt.client.Tracer;
 import org.fusesource.mqtt.codec.MQTTFrame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class SystemOutTracer extends Tracer {
 
+	 private static final Logger LOG = LoggerFactory.getLogger(SystemOutTracer.class);
+	 
 	@Override
 	public void debug(String message, Object... args) {
-		System.out.println(message + " " + args);
+		LOG.debug(message,args);
 	}
 	
 	@Override
 	public void onSend(MQTTFrame frame) {
-		System.out.println(frame);
+		LOG.debug("Client Sent:\n" + frame);
 	}
 	
 	@Override
 	public void onReceive(MQTTFrame frame) {
-		System.out.println(frame);
+		LOG.debug("Client Received:\n" + frame);
 	}
 }
